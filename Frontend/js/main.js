a76f808411559cf37e09ce832610c6c1ddcfce49
steven1046 <stevenzvonek1992@gmail.com>
2018-11-07 14:17:00 -0600

    Add in pom.xml; Update frontend

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -3,20 +3,24 @@ $(document).ready(function() {
     var timestamp_map; // Map of words to timestamp
 
     var uploadFile = function(file){
+        var deferred = new $.Deferred();
         $.ajax({
             url: upload_url,
             type: "POST",
             data: file,
-            success: function(data){
-                if(data){
+            success: function(response){
+                if(response){
                     // Transcript comes back
                 }
+                deferred.resolve();
             },
-            error: function(response){
+            error: function(error){
                 // Display error
+                deferred.reject(error);
             }
         });
-    };    
+        return deferred;
+    };
 
     $('#file_browse').on('change', function(){
         // Insert the uploaded audio file in the audio portion.
@@ -27,10 +31,11 @@ $(document).ready(function() {
         reader.onload = function(event) {
             audio.src = this.result;
             audio.controls = true;
-            // audio.play();
         };
         reader.readAsDataURL(this.files[0]);
-        uploadFile(this.files[0]);
+        uploadFile(this.files[0]).then(function(result){
+            // Show transcript then add 
+        });
     });
 
     // Upon clicking the file upload icon, click the hidden file input to open up a finder.
@@ -40,15 +45,19 @@ $(document).ready(function() {
     });
 
     // Handle clicking on part of the transcript and jumping to the respective spot in the audio clip.
-    $('#transcript span').on('click', function(){
-        var span = this;
-        var text = span.innerText;
-        console.log("Clicked on '" + text + "' in the transcript.");
+    $('#transcript').on('click', function(event){
+        if($(event.target).is('span')){
+            var span = event.target;
+            var text = span.innerText;
+            console.log("Clicked on '" + text + "' in the transcript.");
 
-        var index = span.data('index');
-        var time = timestamp_map[index];
-        if(time){
-            $('#audio')[0].currentTime = time;
-        }        
+            var index = $(span).data('index');
+            if(index){
+                var time = timestamp_map[index];
+                if(time){
+                    $('#audio')[0].currentTime = time;
+                }
+            }  
+        }                      
     });
 });

9441246b6120447ee514a66d89738199317af224
steven1046 <stevenzvonek1992@gmail.com>
2018-11-07 11:53:36 -0600

    Add the audio object to the web app when the user uploads a file; Insert logic for clicking on the transript words and have it jump to a certain time in the video

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    let upload_url = "";
+    var upload_url = "";
+    var timestamp_map;
     $.ajax({
         url: upload_url
     }).done(function(data) {
@@ -11,9 +12,31 @@ $(document).ready(function() {
         }
     });
 
+    $('#file_browse').on('change', function(event){
+        // Insert the uploaded audio file in the audio portion.
+        $('#audio').removeClass('hidden');
+        
+        var audio = document.getElementById('audio');
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            audio.src = this.result;
+            audio.controls = true;
+            // audio.play();
+        };
+        reader.readAsDataURL(this.files[0]);
+    });
+
     // Upon clicking the file upload icon, click the hidden file input to open up a finder.
     $('#file_upload').on('click', function(event){
         event.stopPropagation();
         $('#file_browse').click();
+    });
+
+    // Handle clicking on part of the transcript and jumping to the respective spot in the audio clip.
+    $('#transcript span').on('click', function(){
+        var span = this;
+        console.log("Clicked on '" + span.innerText + "' in the transcript.");
+
+        $('#audio')[0].currentTime = 5;
     });
 });

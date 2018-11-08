46d6edff831dd2d7b5ccca91dbe25ff479067029
dshimo <dshi@utexas.edu>
2018-11-08 09:05:45 -0600

    post search phrase endpoint

--- src/main/java/com/speechscrubber/RevSpeechService.java
+++ src/main/java/com/speechscrubber/RevSpeechService.java
@@ -1,8 +1,12 @@
 package com.speechscrubber;
 
+import javax.json.JsonObject;
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
@@ -10,6 +14,8 @@ import com.speechscrubber.job.JobChecker;
 
 @Path("/speech")
 public class RevSpeechService {
+    String searchPhrase;
+    JobChecker jc = new JobChecker();
 
     @GET
     @Path("/health")
@@ -22,15 +28,29 @@ public class RevSpeechService {
     @Path("/check/{id}")
     @Produces(MediaType.TEXT_PLAIN)
     public String isTranscriptReady(@PathParam("id") String id) {
-        JobChecker jobChecker = new JobChecker();
-        return "id " + id + " ready to transcribe: " + jobChecker.isTranscriptReady(id);
+        return "id " + id + " ready to transcribe: " + jc.isTranscriptReady(id);
     }
 
     @GET
     @Path("/{id}/transcript")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getTranscript(@PathParam("id") String id) throws Exception {
+        return jobChecker.getTranscript(id); 
+    }
+
+    @POST
+    @Path("/search")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getTranscript(@PathParam("id") String id) throws Exception {
-        JobChecker jobChecker = new JobChecker();
-        return "id" + id + " transcript = " + jobChecker.getTranscript(id); 
+    public String makeSearchPhrase(@QueryParam("phrase") String phrase) {
+        setSearchPhrase(phrase);
+        return "passed phrase: " + getSearchPhrase();
+    }
+
+    private String setSearchPhrase(String phrase) {
+        this.searchPhrase = phrase;
+    }
+
+    private String getSearchPhrase() {
+        return this.searchPhrase;
     }
 }

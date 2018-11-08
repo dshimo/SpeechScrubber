d6b2961c7d2d743e8e8ee6f2a36d9bf5c9f5bda9
Adam Yoho <ayoho@us.ibm.com>
2018-11-08 09:09:39 -0600

    Update API key environment var name

--- src/main/java/com/speechscrubber/Constants.java
+++ src/main/java/com/speechscrubber/Constants.java
@@ -3,6 +3,6 @@ package com.speechscrubber;
 public class Constants {
 
     public static final String BASE_URL = "https://api.rev.ai/revspeech/v1beta";
-    public static final String API_KEY_SYSTEM_PROP = "rev.api.key";
+    public static final String API_KEY_SYSTEM_PROP = "REV_API_KEY";
 
 }
